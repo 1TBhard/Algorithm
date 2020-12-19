6d78f266860266ee21bd00f37d7ab79e81442f47
1TBhard <was1383@naver.com>
2020-12-19 20:33:58 +0900

    백준 2589 수정

--- 백준/js/2589.js
+++ 백준/js/2589.js
@@ -7,84 +7,81 @@
 /* ============================================================== */
 
 function solution(LIMIT_H, LIMIT_W, map) {
-  let answer = 0;
-  /**
+	let answer = 0;
+	/**
     @param {
       h: 세로 위치
       w: 가로 위치
     }
   */
-  
 
-  const dfs = (startX, startY) => {
+	const dfs = (startX, startY) => {
+		let copyMap = [...map.map((_) => [..._])];
 
-    let copyMap = [...map.map(_ => [..._])];
+		// 처음 시작점을 S로 잡음
+		copyMap[startX][startY] = "S";
 
-    // 처음 시작점을 S로 잡음
-    copyMap[startX][startY] = "S";
+		const isCanGo = (y, x, cost) => {
+			// 높이 범위를 초과
+			if (y < 0 || y >= LIMIT_H) return false;
 
-    const isCanGo = (y, x, cost) => {
-      // 높이 범위를 초과
-      if(y < 0 || y >= LIMIT_H) return false;
-  
-      // 너비 범위를 초과
-      if(x < 0 || x >= LIMIT_W) return false;
-  
-      // 현위치가 이미 방문한 땅일 때, 코스트가 그 땅보다 높은 경우
-      if(copyMap[y][x] !== "L" || copyMap[y][x] <= cost) return false;
-  
-      return true;
-    }
+			// 너비 범위를 초과
+			if (x < 0 || x >= LIMIT_W) return false;
 
-    // 다음 방문할 땅 (y, x)가 있는 큐
-    let queue = [[startX, startY, 0]];
+			// 현위치가 이미 방문한 땅일 때, 코스트가 그 땅보다 높은 경우
+			if (copyMap[y][x] !== "L" || copyMap[y][x] <= cost) return false;
 
-    while(queue.length > 0) {
-      var [y, x, step] = queue.shift();
+			return true;
+		};
 
-      answer = Math.max(answer, step);
+		// 다음 방문할 땅 (y, x)가 있는 큐
+		let queue = [[startY, startX, 0]];
 
-      var canGoUp = isCanGo(y-1, x, step + 1);
-      var canGoDown = isCanGo(y+1, x, step + 1);
-      var canGoLeft = isCanGo(y, x-1, step + 1);
-      var canGoRight = isCanGo(y, x+1, step + 1);
+		while (queue.length > 0) {
+			var [y, x, step] = queue.shift();
 
-      if(canGoUp) {
-        queue.push([y-1, x, step + 1]);
-        copyMap[y-1][x] = step + 1;
-      }
+			answer = Math.max(answer, step);
 
-      if(canGoDown) {
-        queue.push([y+1, x, step + 1]);
-        copyMap[y+1][x] = step + 1;
-      }
+			var canGoUp = isCanGo(y - 1, x, step + 1);
+			var canGoDown = isCanGo(y + 1, x, step + 1);
+			var canGoLeft = isCanGo(y, x - 1, step + 1);
+			var canGoRight = isCanGo(y, x + 1, step + 1);
 
-      if(canGoLeft) {
-        queue.push([y, x-1, step + 1]);
-        copyMap[y][x-1] = step + 1;
-      }
+			if (canGoUp) {
+				queue.push([y - 1, x, step + 1]);
+				copyMap[y - 1][x] = step + 1;
+			}
 
-      if(canGoRight) {
-        queue.push([y, x+1, step + 1]);
-        copyMap[y][x+1] = step + 1;
-      }
-    }
-  }
+			if (canGoDown) {
+				queue.push([y + 1, x, step + 1]);
+				copyMap[y + 1][x] = step + 1;
+			}
 
+			if (canGoLeft) {
+				queue.push([y, x - 1, step + 1]);
+				copyMap[y][x - 1] = step + 1;
+			}
 
-  for(var startY=0 ; startY < LIMIT_H ; startY++) {
-    for(var startX=0 ; startX < LIMIT_W ; startX++) {
-      switch(map[startY][startX]) {
-        case 'W':
-          break;
-        case 'L':
-          dfs(startY, startX);
-          break;
-      }
-    }
-  }
+			if (canGoRight) {
+				queue.push([y, x + 1, step + 1]);
+				copyMap[y][x + 1] = step + 1;
+			}
+		}
+	};
 
-  console.log(answer);
+	for (var startY = 0; startY < LIMIT_H; startY++) {
+		for (var startX = 0; startX < LIMIT_W; startX++) {
+			switch (map[startY][startX]) {
+				case "W":
+					break;
+				case "L":
+					dfs(startY, startX);
+					break;
+			}
+		}
+	}
+
+	console.log(answer);
 }
 
 const input = `3 3
@@ -93,8 +90,8 @@ WWW
 WLL`;
 
 let [POS, ...MAP] = input.split("\n");
-const [HEIGHT, WIDTH] = POS.split(" ").map(ch => parseInt(ch));
+const [HEIGHT, WIDTH] = POS.split(" ").map((ch) => parseInt(ch));
 
-MAP = MAP.map( _ => _.split(""));
+MAP = MAP.map((_) => _.split(""));
 
-solution(HEIGHT, WIDTH, MAP)
+solution(HEIGHT, WIDTH, MAP);
