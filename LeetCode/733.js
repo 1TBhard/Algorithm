8c1c21f8f0fd8de939970897301ab52b8b9cdd54
1TBhard <was1383@naver.com>
2022-12-18 16:40:55 +0900

    solve 733

--- LeetCode/733.js
+++ LeetCode/733.js
@@ -5,4 +5,35 @@
  * @param {number} color
  * @return {number[][]}
  */
-var floodFill = function (image, sr, sc, color) {};
+var floodFill = function (image, sr, sc, color) {
+	const q = [[sr, sc]];
+	const beforeColor = image[sr][sc];
+	image[sr][sc] = color;
+
+	const getGoTo = (y, x) => [
+		[y + 1, x],
+		[y - 1, x],
+		[y, x + 1],
+		[y, x - 1],
+	];
+
+	while (q.length) {
+		let [cur_y, cur_x] = q.shift();
+
+		for ([y, x] of getGoTo(cur_y, cur_x)) {
+			if (y < 0 || y >= image.length || x < 0 || x >= image[0].length) {
+				continue;
+			}
+
+			if (image[y][x] !== beforeColor || image[y][x] === color) {
+				continue;
+			}
+
+			image[y][x] = color;
+
+			q.push([y, x]);
+		}
+	}
+
+	return image;
+};
